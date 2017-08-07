a03495004935b468c2a6c7aabef05b0e91860dbf
Mack-Kali <mack.preston42@gmail.com>
2017-08-07 16:13:08 -0400

    Updated init.py to copy .vimrc

--- init.py
+++ init.py
@@ -1,9 +1,14 @@
 # init.py - Initialize workspace with custom .vimrc
+
 import os
+import subprocess
 
 assert os.path.exists(".vimrc")
 
 if os.name == "nt":
-    print 'windows'
+    print '[~] windows detected - no windows functionality has been added yet... '
+    sys.exit(1)
 else:
-    print 'unix!'
+    print '[~] unix!'
+    print '[bash] cp .vimrc ~/.vimrc'
+    subprocess.call("cp .vimrc ~/.vimrc", shell=True, stdout=subprocess.PIPE)
